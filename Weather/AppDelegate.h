47763f43645b200d34d511a549fada8e7a0e921d
heqingbao <andro.he@gmail.com>
2014-06-28 10:59:29 +0800

    Initial Commit

---
+++ Weather/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Weather
+//
+//  Created by HeQingbao on 14-6-28.
+//  Copyright (c) 2014年 HeQingbao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Weather/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Weather
+//
+//  Created by HeQingbao on 14-6-28.
+//  Copyright (c) 2014年 HeQingbao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
